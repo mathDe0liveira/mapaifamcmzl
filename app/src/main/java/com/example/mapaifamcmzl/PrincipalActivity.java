9efcea7ccaa9f48249a979eeae328d93f00f6fc9
Matheus Andrade <matheusandrade1706@gmail.com>
2024-12-20 00:22:42 -0300

    tela ct aranoua, tela mapa e intents

--- app/src/main/java/com/example/mapaifamcmzl/PrincipalActivity.java
+++ app/src/main/java/com/example/mapaifamcmzl/PrincipalActivity.java
@@ -1,6 +1,10 @@
 package com.example.mapaifamcmzl;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    private Button buttonCTAranoua;
+    private ImageButton imageButtonMapa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
+
+        buttonCTAranoua = findViewById(R.id.buttonCTAranoua);
+        imageButtonMapa = findViewById(R.id.imageButtonMapa);
+
+        buttonCTAranoua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                telaCTAranoua();
+            }
+        });
+
+        imageButtonMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                telaMapa();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void telaMapa() {
+        startActivity(new Intent(this, MapaActivity.class));
+    }
+
+    private void telaCTAranoua() {
+        startActivity(new Intent(this, CTAranouaActivity.class));
     }
 }
